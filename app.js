cbf7fc29e8215df02eda81c09c7a906a63cb2191
cjf8140 <32036089+cjf8140@users.noreply.github.com>
2022-09-05 18:26:32 +0900

    a

--- app.js
+++ app.js
@@ -538,7 +538,7 @@ function allD(ch, ms) {
 
 
 // client.login(process.env.TOKEN);
-client.login('ODE2Mjg4NTc3MDI1MDgxMzQ0.YD4x-g.nuFX8V0I7JeQKWVsOe8SjVOi8u8');
+client.login('TOKEN');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! `, version);
