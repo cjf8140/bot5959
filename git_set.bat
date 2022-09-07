902713bce632b96462802fd9c2a1a02c9ec82955
cjf8140 <32036089+cjf8140@users.noreply.github.com>
2022-09-07 16:08:38 +0900

    a

--- git_set.bat
+++
@@ -1,4 +0,0 @@
-git remote add one https://git.heroku.com/bot5959.git
-git remote add two https://git.heroku.com/bot5959-after.git
-git remote add origin https://github.com/cjf8140/bot5959.git
-git remote -v
--- git_update.bat
+++
@@ -1,5 +0,0 @@
-git add .
-git commit -am upload
-git push one master
-git push two master
-git push origin master
